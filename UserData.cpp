98247c955933cf0a98788b12196f27afe2eafcd2
legojrp <84683257+legojrp@users.noreply.github.com>
2021-07-31 20:48:38 -0400

    v1.0-beta.1

--- UserData.cpp
+++ UserData.cpp
@@ -1,18 +1,21 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <cstring>
 class UserData { 
 public:
-	UserData(path){};
-	void writeToProperty();
+	UserData(std::string path){
+      this->path = path;
+    }
+	void writeToProperty(std::string Property, std::string Replacement);
 	std::string readFromProperty(std::string Property);
-	void createFile();
-	void changeProperty();
+	void createSubset(); 
+	void createProperty(std::string Property, std::string Replacement);
 private:
 	std::string path;
 };
 std::string UserData::readFromProperty(std::string Property){
-  std::ofstream file(this->path);
+  std::ifstream file(this->path);
   std::string text;
   std::string returnstring = "";
    while (std::getline(file, text)){
@@ -28,26 +31,31 @@ std::string UserData::readFromProperty(std::string Property){
   return returnstring;
 }
 void UserData::writeToProperty(std::string Property, std::string Replacement){
-	std:ofstream file(this->path);
+	std::ifstream file(this->path);
   	std::string text, pre, post, changed;    
     int preOrPost = 0;
     while (std::getline(file, text)){
       if (text.substr(0, text.find(": ")) == Property){
-        int = 1;
+         preOrPost= 1;
         continue;
       }
-      else if (int == 0){
+      else if ( preOrPost == 0){
         pre += text + "\n";
       }
-      else if (int == 1){
+      else if (preOrPost == 1){
         post += text + "\n";
       }
 	
     }
-    changed = pre + Property + ": " + Replacement + post;
+    changed = pre + Property + ": " + Replacement + "\n" + post;
     file.close();
-    std::remove(this->path);
-    std::ifstream file(this->path);
-    file << changed;
-    file.close()
+    std::remove(this->path.c_str());
+    std::ofstream file2(this->path);
+    file2 << changed;
+    file2.close();
+}
+void createProperty(std::string Property, std::string Replacement){
+  	std::ofstream file(this->path);
+    file << Property + ": " + Value + "\n";
+    file.close();
 }
